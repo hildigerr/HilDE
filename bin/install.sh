56b87af7bc7cdeed64a0995f60e556dd7514bb21
R.M. HildigerR V <moonsdad@gmail.com>
2016-07-03 23:26:45 -0700

    Shutdown capability and Basic security settings: users group and umask.

--- bin/install.sh
+++ bin/install.sh
@@ -32,3 +32,22 @@ pushd . && cd .. #Expecting to called from within HilDE/bin/
   make install && cp -r /usr/share/lxpanel/profile/hilde $HOME/.config/default
   cp `which hilde-start` $HOME/.Xsession
 popd
+
+# Basic Default Security Settings
+# Logging and UMASK
+sed -i.old-`date +%F` -e "s/^#SULOG_FILE/SULOG_FILE/" /etc/login.defs
+sed -i -e "s/^UMASK.*$/UMASK\t\t077/" /etc/login.defs
+sed -i -e "s/#umask 022/#umask 077/" /etc/skel/.profile
+
+# Use users Group
+sed -i -e "s/USERGROUPS_ENAB yes/USERGROUPS_ENAB no/" /etc/login.defs
+sed -i -e "s/^# GROUP=100/GROUP=100/" /etc/default/useradd
+usermod -g users $USER && gpasswd -d $USER $USER && delgroup $USER
+chmod -R g-w $HOME
+
+# users can shutdown and reboot
+echo |                       (EDITOR="tee -a" visudo)
+echo "#Users can shutdown" | (EDITOR="tee -a" visudo)
+echo "%users ALL =NOPASSWD: /sbin/poweroff,/sbin/shutdown,/sbin/reboot" | \
+                             (EDITOR="tee -a" visudo)
+
--- bin/stop.sh
+++ bin/stop.sh
@@ -1,2 +1,19 @@
 #!/bin/sh
-killall -u `printenv USER`
+
+Cancel=1
+Reboot=2
+Shutdown=3
+Logout=4
+
+xmessage "Exit Methods:" -center -title "Exit Methods" -default "Cancel" -buttons "Cancel":$Cancel,"Reboot":$Reboot,"Shutdown":$Shutdown,"Logout":$Logout
+
+case $? in
+    1) #Cancel
+        echo "Exit Canceled by $USER [`date`]";;
+    2) #
+        sudo reboot;;
+    3) #
+        sudo shutdown -h now;;
+    4) #Logout
+        killall -u `printenv USER`;;
+esac
