0d873c0b930750b36371cf060c770edf0ab15ed7
Hildigerr Vergaray <Hildigerr.Vergaray@YmirSystems.com>
2025-02-15 19:57:24 -0800

    warn user if not using XDG config directory for xbindkeysrc

--- files/etc/X11/xinit/xinitrc.d/99-spawn-xbindkeys.sh
+++ files/etc/X11/xinit/xinitrc.d/99-spawn-xbindkeys.sh
@@ -3,6 +3,9 @@
 config_file="${XDG_CONFIG_HOME:-$HOME/.config}/xbindkeysrc"
 if [ ! -f "$config_file" ]; then
   if [ -f "$HOME/.xbindkeysrc" ]; then
+    echo "Warning: For better organization, you can move your .xbindkeysrc into your XDG config directory:"
+    echo "     mv \"$HOME/.xbindkeysrc\" \"$config_file\""
+    echo "This will move it to: \"$config_file\""
     config_file="$HOME/.xbindkeysrc"
   else
     config_file="/etc/xdg/hilde/xbindkeysrc"
